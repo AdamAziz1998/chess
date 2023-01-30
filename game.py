50d4d28bd223e225eb129d115fdd47f53bfcc21f
Adam O'Neill <adamoneill@Adams-MacBook-Pro.local>
2023-01-30 00:48:12 +0000

    some pygame file changes

--- game.py
+++ game.py
@@ -19,24 +19,34 @@ from pieces import (
 
 pygame.init()
 
-WIN = pygame.display.set_mode((640, 640))
+WIDTH = 640
+HIEGHT = 640
+WIN = pygame.display.set_mode((WIDTH, HIEGHT))
 pygame.display.set_caption("Chess")
 FPS = 60
 
+BACKGROUND = pygame.transform.scale(pygame.image.load('images', 'board.png'), (WIDTH, HIEGHT))
+
+location_array = [(0,0) for i in range(12)]
+def blit_pieces(location_array): #this will be in the order as the pieces below
+
+    WIN.blit(WHITE_KING, (location_array[0][0], location_array[0][1]))
+    WIN.blit(WHITE_QUEEN, (location_array[1][0], location_array[1][1]))
+    WIN.blit(WHITE_ROOK, (location_array[2][0], location_array[2][1]))
+    WIN.blit(WHITE_KNIGHT, (location_array[3][0], location_array[3][1]))
+    WIN.blit(WHITE_BISHOP, (location_array[4][0], location_array[4][1]))
+    WIN.blit(WHITE_PAWN, (location_array[5][0], location_array[5][1]))
+    WIN.blit(BLACK_KING, (location_array[6][0], location_array[6][1]))
+    WIN.blit(BLACK_QUEEN, (location_array[7][0], location_array[7][1]))
+    WIN.blit(BLACK_ROOK, (location_array[8][0], location_array[8][1]))
+    WIN.blit(BLACK_KNIGHT, (location_array[9][0], location_array[9][1]))
+    WIN.blit(BLACK_BISHOP, (location_array[10][0], location_array[10][1]))
+    WIN.blit(BLACK_PAWN, (location_array[11][0], location_array[11][1]))
+
+
 def draw_window():
-    WIN.fill((0,100,100))
-    WIN.blit(WHITE_KING, (0, 0))
-    WIN.blit(WHITE_QUEEN, (0, 0))
-    WIN.blit(WHITE_ROOK, (0, 0))
-    WIN.blit(WHITE_KNIGHT, (0, 0))
-    WIN.blit(WHITE_BISHOP, (0, 0))
-    WIN.blit(WHITE_PAWN, (0, 0))
-    WIN.blit(BLACK_KING, (0, 0))
-    WIN.blit(BLACK_QUEEN, (0, 0))
-    WIN.blit(BLACK_ROOK, (0, 0))
-    WIN.blit(BLACK_KNIGHT, (0, 0))
-    WIN.blit(BLACK_BISHOP, (0, 0))
-    WIN.blit(BLACK_PAWN, (0, 0))
+    WIN.blit(BACKGROUND, (0, 0))
+    blit_pieces(location_array)
     
 
     pygame.display.update()
