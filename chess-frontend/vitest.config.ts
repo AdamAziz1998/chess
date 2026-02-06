c4cca742d949e38c5520de26fdbe17a039cee3db
Adam ONeill <adamaoneill@gmail.com>
2026-02-06 14:28:10 +0000

    Add config for vitest

--- chess-frontend/vitest.config.ts
+++ chess-frontend/vitest.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vitest/config';
+import {playwright} from '@vitest/browser-playwright';
 
 export default defineConfig({
   test: {
     browser: {
       enabled: true,
-      instances: [
-        {
-          browser: 'chrome',
-          setupFiles: [],
+      provider: playwright({
+        launchOptions: {
+          args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+          ],
         },
-      ],
+      }),
+      instances: [{ browser: 'chromium' }],
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
-    },
-  },
+      },
+    }
 });
